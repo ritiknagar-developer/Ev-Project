903b0571ac538431b2aabca8003a0800a043ac17
ritiknagar-developer <iamritiknagar@gmail.com>
2025-03-08 02:17:39 +0530

    add photo

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,7 +1,7 @@
 import './Hero.css'
-import arrow_btn from '../../Assets/arrow_btn.png'
-import pause_icon from '../../Assets/pause_icon.png'
-import play_icon from '../../Assets/play_icon.png'
+import arrow_btn from '../../assets/arrow_btn.png'
+import pause_icon from '../../assets/pause_icon.png'
+import play_icon from '../../assets/play_icon.png'
 
 const Hero = ({heroData,setHeroCount,heroCount,playStatus,setPlayStatus}) => {  
   return (
